499327c5a2ebd559ab7c0e12445dce239f917788
Abdul Azees <codeforall39@gmail.com>
2024-07-08 14:23:18 -0700

    add Product Model

--- oruchai/Produts/models.py
+++ oruchai/Produts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# Products Models 
+
+class Product(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES =((LIVE,'Live'),(DELETE,'Delete'))
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField()
+    image =models.ImageField()
+    priority = models.IntegerField(default=0) # priority to display product
+    delete_status = models.IntegerField(choices=DELETE_CHOICES,default=LIVE) # to determine delete product or not
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) # some of the standrad Practice
+
+    def __str__(self) -> str:
+        return self.title # string represention
+
+
+
+
+
+
